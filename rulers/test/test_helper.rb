5d49adb42895c2942d4d1b456fde86fdfb28e30f
dave doolin <david.doolin@gmail.com>
2017-12-16 10:17:21 -0800

    p. 26 - first test for rulers gem

---
+++ rulers/test/test_helper.rb
@@ -0,0 +1,8 @@
+require 'rack/test'
+require 'test/unit'
+
+# Use local rulers first:
+d = File.join(File.dirname(__FILE__), "..", 'lib')
+$LOAD_PATH.unshift(File.expand_path(d))
+
+require 'rulers'
