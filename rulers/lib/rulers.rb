b48edb3d2c298020d36be09488d2efe2bf16e451
dave doolin <david.doolin@gmail.com>
2017-12-16 07:36:50 -0800

    p. 23 - gemspec now working correctly
    rubocop is versioned, gem builds and installs.

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,8 +1,10 @@
+require "rulers/array"
 require "rulers/version"
 
 module Rulers
   class Application
     def call(env)
+      `echo debug > debug.txt`
       [200, { 'Content-Type' => 'text/html' },
        ['Hello from Ruby on Rulers!']]
     end
