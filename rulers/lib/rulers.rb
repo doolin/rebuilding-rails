93503463723a86fdd63daca78b332ab56f707e75
dave doolin <david.doolin@gmail.com>
2017-12-24 07:16:16 -0800

    p. 35 - punt on favicon for now

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -5,11 +5,14 @@ require 'rulers/routing'
 module Rulers
   class Application
     def call(env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, { 'Content-Type' => 'text/html' }, []]
+      end
+
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
       text = controller.send(act)
-      [200, { 'Content-Type' => 'text/html' },
-       [text]]
+      [200, { 'Content-Type' => 'text/html' }, [text]]
     end
   end
 
