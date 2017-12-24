b0016d9867182d1010d7057385c2e5da8a1f28b8
dave doolin <david.doolin@gmail.com>
2017-12-24 07:26:48 -0800

    p. 37 - show the rack env values

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,5 +1,6 @@
 class QuotesController < Rulers::Controller
   def a_quote
-    'There is nothing good or bad but thinking makes it so.'
+    'There is nothing good or bad but thinking makes it so.' +
+      "\n<pre>\n#{env}\n</pre>"
   end
 end
