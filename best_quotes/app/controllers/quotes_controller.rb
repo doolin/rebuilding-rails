df800a624f4c8feea36834282ec8847a40899a26
dave doolin <david.doolin@gmail.com>
2018-02-18 06:11:45 -0800

    p. 60 render a view

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -3,6 +3,10 @@ require 'json'
 
 class QuotesController < Rulers::Controller
   def a_quote
+    render :a_quote, noun: :winking
+  end
+
+  def environment
     env[:quote] = 'There is nothing good or bad but thinking makes it so.'
     "\n<pre>\n#{env.to_json}\n</pre>"
   end
