fe245907bb751fff29013e44f97ad400e2bb2c2f
dave doolin <david.doolin@gmail.com>
2018-02-21 04:14:09 -0800

    p. 62 update tests

--- rulers/test/application_test.rb
+++ rulers/test/application_test.rb
@@ -1,6 +1,15 @@
 require_relative './test_helper'
 
+class TestController < Rulers::Controller
+  def index
+    "Hello!"
+  end
+end
+
 class TestApp < Rulers::Application
+  def get_controller_and_action(env)
+    [TestController, "index"]
+  end
 end
 
 class RulersAppTest < Test::Unit::TestCase
