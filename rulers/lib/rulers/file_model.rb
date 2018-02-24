73358d31ff7cea2c080d64e06f50120db86b87d4
dave doolin <david.doolin@gmail.com>
2018-02-24 06:14:39 -0800

    p. 71 FileModel

---
+++ rulers/lib/rulers/file_model.rb
@@ -0,0 +1,31 @@
+require 'multi_json'
+
+module Rulers
+  module Model
+    class FileModel
+      def initialize(filename)
+        @filename = filename
+        # If filename is dir/37.json, @id is 37
+        basename = File.split(filename)[-1]
+        @id = File.basename(basename, ".json").to_i
+
+        obj = File.read(filename)
+        @hash = MultiJson.load(obj)
+      end
+
+      def [](name)
+        @hash[name.to_s]
+      end
+
+      def []=(name, value)
+        @hash[name.to_s] = value
+      end
+
+      def self.find(id)
+        FileModel.new("db/quotes/#{id}.json")
+      rescue
+        nil
+      end
+    end
+  end
+end
