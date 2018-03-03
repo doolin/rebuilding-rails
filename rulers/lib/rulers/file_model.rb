3336de93ae18ca4893cbea434a9edb9cc7d9e21e
dave doolin <david.doolin@gmail.com>
2018-03-03 06:58:20 -0800

    p. 76 create method for quote file

--- rulers/lib/rulers/file_model.rb
+++ rulers/lib/rulers/file_model.rb
@@ -31,6 +31,30 @@ module Rulers
         files = Dir["db/quotes/*.json"]
         files.map { |f| FileModel.new f }
       end
+
+      def self.create(attrs)
+        hash = {}
+        hash['submitter'] = attrs['submitter'] || ''
+        hash['quote'] = attrs['quote'] || ''
+        hash['attribution'] = attrs['attribution'] || ''
+
+        files = Dir['db/quotes/*.json']
+        names = files.map { |f| File.split(f)[-1] }
+        highest = name.map { |b| b.to_i }.max
+        id = highest + 1
+
+        File.open("db/quotes/#{id}.json", 'w') do |f|
+          f.write <<TEMPLATE
+          {
+            "submitter": "#{hash["submitter"]}",
+            "quote": "#{hash["quote"]}",
+            "attribution": "#{hash["attribution"]}"
+          }
+TEMPLATE
+        end
+
+        FileModel.new("db/quotes/#{id}.json")
+      end
     end
   end
 end
