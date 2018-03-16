9634082f74f897794bb488eaa0f33dc286c980df
dave doolin <david.doolin@gmail.com>
2018-03-16 04:46:33 -0700

    p. 94 schema method

--- rulers/lib/rulers/sqlite_model.rb
+++ rulers/lib/rulers/sqlite_model.rb
@@ -9,6 +9,15 @@ module Rulers
       def self.table
         Rulers.to_underscore(name)
       end
+
+      def self.schema
+        return @schema if @schema
+        @schema = {}
+        DB.table_info(table) do |row|
+          @schema[row['name']] = row['type']
+        end
+        @schema
+      end
     end
   end
 end
