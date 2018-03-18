c038dc33080d4180cb1fdf26d4027ac54ae8bd97
dave doolin <david.doolin@gmail.com>
2018-03-18 05:05:42 -0700

    p. 95 sqlite to_sql

--- rulers/lib/rulers/sqlite_model.rb
+++ rulers/lib/rulers/sqlite_model.rb
@@ -6,6 +6,23 @@ DB = SQLite3::Database.new('test.db')
 module Rulers
   module Model
     class SQLite3
+      def initialize(data = nil)
+        @hash = data
+      end
+
+      def self.to_sql(val)
+        case val
+        when NilClass
+          'null'
+        when Numeric
+          val.to_s
+        when String
+          "'#{val}'"
+        else
+          raise "Can't change #{val.class} to SQL!"
+        end
+      end
+
       def self.table
         Rulers.to_underscore(name)
       end
