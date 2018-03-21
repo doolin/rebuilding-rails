dd7395828210ed3cb3f1d6a72bce6b45cabfa024
dave doolin <david.doolin@gmail.com>
2018-03-21 04:25:03 -0700

    p. 97 database example script

--- best_quotes/sqlite_test.rb
+++ best_quotes/sqlite_test.rb
@@ -3,3 +3,10 @@ require 'rulers/sqlite_model'
 
 class MyTable < Rulers::Model::SQLite3; end
 STDERR.puts MyTable.schema.inspect
+
+mt = MyTable.create('title' => 'It happened!',
+                    'posted' => 1,
+                    'body' => 'It did!')
+mt = MyTable.create('title' => 'I saw it!')
+
+puts "Count: #{MyTable.count}"
