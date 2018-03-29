3ccdb29cfe2d88ab58b778adac9875b94944b7c3
dave doolin <david.doolin@gmail.com>
2018-03-29 04:01:52 -0700

    p. 101 save method for sqlite

--- rulers/lib/rulers/sqlite_model.rb
+++ rulers/lib/rulers/sqlite_model.rb
@@ -23,6 +23,30 @@ module Rulers
         end
       end
 
+      def save!
+        unless @hash['id']
+          self.class.create
+          return true
+        end
+
+        fields = @hash.map do |k, v|
+          "#{k}=#{self.class.to_sql(v)}"
+        end.join(',')
+
+        DB.execute <<-SQL
+          UPDATE #{self.class.table}
+          SET #{fields}
+          WHERE id = #{@hash['id']}
+        SQL
+        true
+      end
+
+      def save
+        self.save!
+      rescue
+        false
+      end
+
       def self.find(id)
         row = DB.execute(<<-SQL)
         SELECT #{schema.keys.join(',')} FROM #{table}
