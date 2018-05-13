d9085081da7c0b4f89229872cd8e5aed4ad89d9f
dave doolin <david.doolin@gmail.com>
2018-05-13 06:38:38 -0700

    more specs for rulers::application

--- rulers/spec/rulers_spec.rb
+++ rulers/spec/rulers_spec.rb
@@ -2,4 +2,22 @@ RSpec.describe Rulers do
   it "has a version number" do
     expect(Rulers::VERSION).not_to be nil
   end
+
+  describe Rulers::Application do
+    describe '.new' do
+      it 'instantiates' do
+        expect(Rulers::Application.new).to_not be_nil
+      end
+    end
+
+    describe '#call' do
+      it '' do
+        env = { 'PATH_INFO' => '/favicon.ico' }
+        app = Rulers::Application.new
+        expected = [404, {"Content-Type"=>"text/html"}, []]
+
+        expect(app.call(env)).to eq expected
+      end
+    end
+  end
 end
