1d487596557ad96a0d1b0e95e5cd59a5a9a3f365
dave doolin <david.doolin@gmail.com>
2018-05-19 04:48:33 -0700

    more routing tests

--- rulers/spec/routing_spec.rb
+++ rulers/spec/routing_spec.rb
@@ -1,5 +1,20 @@
 module Rulers
   RSpec.describe RouteObject do
+=begin
+app.route do
+  match '', 'quotes#index'
+  match 'sub-app',
+    proc { [200, {}, ['Hello, sub-app!']] }
+
+  # default routes
+  match ':controller/:id/:action'
+  match ':controller/:id',
+    default: { 'action' => 'show' }
+  match ':controller',
+    default: { 'action' => 'index' }
+end
+=end
+
     describe '#check_url' do
       it '' do
         routing = RouteObject.new
@@ -10,6 +25,19 @@ module Rulers
     end
 
     describe 'match' do
+      it '' do
+        router = RouteObject.new
+        result = router.match('')
+        expected = [
+          {
+            :regexp=>/^\/$/,
+            :vars=>[],
+            :dest=>nil,
+            :options=>{:default=>{}}
+          }
+        ]
+        expect(result).to eq expected
+      end
     end
   end
 end
